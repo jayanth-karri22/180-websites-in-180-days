74b02dfedb95144676204d0c5c319550a05fdc11
smileyKJ <kj.invincible@gmail.com>
2019-02-08 13:06:32 +0530

    make-a-deal

---
+++ Make a Deal/main.js
@@ -0,0 +1,60 @@
+let bluedoor = document.getElementById("blue-door");
+let orangedoor = document.getElementById("orange-door");
+let greendoor = document.getElementById("green-door");
+let header = document.getElementsByTagName("h1");
+let isDoorOpen = false;
+
+let openedDoor = (firstDoor, secondDoor) =>
+  (document.getElementsByClassName("Rules")[0].innerHTML = `
+        <p>I opened the ${secondDoor} door for you which held a BAD THING</p>
+        <p>Now if you want to switch you can choose the ${
+          firstDoor !== "GreenDoor" ? "GreenDoor" : "BlueDoor"
+        }</p>
+        <p>or you can keep your first choice and open the ${firstDoor}</p>
+    `);
+
+bluedoor.addEventListener("click", () =>
+  isDoorOpen == false
+    ? [
+        (header[0].innerText = "You Picked the Blue Door!"),
+        openedDoor("BlueDoor", "OrangeDoor"),
+        (orangedoor.src = "assets/orange_door_fire.png"),
+        (isDoorOpen = true),
+        (orangedoor.disabled = true)
+      ]
+    : [
+        (document.getElementsByClassName(
+          "container"
+        )[0].innerHTML = `<h1>You Lose</h1>`)
+      ]
+);
+orangedoor.addEventListener("click", () =>
+  isDoorOpen == false
+    ? [
+        (header[0].textContent = "You Picked the Orange door"),
+        openedDoor("OrangeDoor", "BlueDoor"),
+        (bluedoor.src = "assets/blue_door_fire.png"),
+        (isDoorOpen = true),
+        (bluedoor.disabled = true)
+      ]
+    : [
+        (document.getElementsByClassName(
+          "container"
+        )[0].innerHTML = `<h1>You Lose</h1>`)
+      ]
+);
+greendoor.addEventListener("click", () =>
+  isDoorOpen == false
+    ? [
+        (header[0].textContent = "You Picked the Green door"),
+        openedDoor("GreenDoor", "OrangeDoor"),
+        (orangedoor.src = "assets/orange_door_fire.png"),
+        (isDoorOpen = true),
+        (orangedoor.disabled = true)
+      ]
+    : [
+        (document.getElementsByClassName(
+          "container"
+        )[0].innerHTML = `<h1>You Win</h1>`)
+      ]
+);
