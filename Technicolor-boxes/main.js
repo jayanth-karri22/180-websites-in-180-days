1bb641f4cad6a5555ed9140354fb0a108dc1f4f6
smileyKJ <kj.invincible@gmail.com>
2019-02-12 14:45:12 +0530

    Technicolor boxes

---
+++ Technicolor-boxes/main.js
@@ -0,0 +1,14 @@
+let container = document.getElementsByClassName("container")[0];
+
+let randomRGB = () => {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+
+  return `rgb(${r},${g},${b})`;
+};
+
+container.addEventListener(
+  "mouseover",
+  e => (e.target.style.background = randomRGB())
+);
