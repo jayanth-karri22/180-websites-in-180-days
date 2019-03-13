b291dfb43d2adf95c34df9be517a5460aa997ca8
smileyKJ <kj.invincible@gmail.com>
2019-03-13 18:52:51 +0530

    bug fix

--- Teleprompter/teleprompt.js
+++ Teleprompter/teleprompt.js
@@ -2,17 +2,17 @@ let content = document.getElementsByTagName("h1")[0];
 
 let value = window.location.search.split("=")[1];
 
-let newValue = value.replace(/[+]/g, " ");
+let newValue = value.replace(/[+,.-_%@!#$%^*()]/g, " ");
 
-newValue == ""
+value = ""
   ? (newValue = "You should probably enter some text next time")
   : newValue;
 
-content.textContent = encodeURI(newValue);
+content.textContent = newValue;
 
 function myMove() {
   var pos = 20;
-  var id = setInterval(frame, 1);
+  var id = setInterval(frame, 10);
   function frame() {
     if (pos == -1000) {
       clearInterval(id);
